eefba46ee73e660da66ee32943d94177ec2f49ca
Yang Theng <yangtheng92@gmail.com>
2017-06-14 17:41:24 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
  var cards = ["queen", "queen", "king", "king"];
  var cardsInPlay = [];
- var cardOne = cards[0];
- cardsInPlay.push(cardOne);
-
- console.log ("User flipped" + " " + cardsInPlay[cardsInPlay.length - 1]);
-
- var cardTwo = cards[1];
- cardsInPlay.push(cardTwo);
-
- console.log ("User flipped" + " " + cardsInPlay[cardsInPlay.length - 1]);
-
- if (cardsInPlay.length === 2) {
+ var checkForMatch = function() {
  	if (cardsInPlay[0] === cardsInPlay[1]) {
- 		alert("You found a match!");
- 	} else {
- 		alert("Sorry, try again!");
- 	}
+	 		alert("You found a match!");
+	 	} else {
+	 		alert("Sorry, try again!");
+	 	}
  }
+
+ var flipCard = function (cardId) {
+	 console.log("User flipped " + cards[cardId]);
+
+	 cardsInPlay.push(cards[cardId]);
+
+	 if (cardsInPlay.length === 2) {
+	 	checkForMatch();
+	 }
+ }
+ 
+ flipCard (0);
+ flipCard (2);
