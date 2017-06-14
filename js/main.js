512518c712613d443ec8b01a46d8cb567de85e37
Yang Theng <yangtheng92@gmail.com>
2017-06-15 01:49:12 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
- var cards = ["queen", "queen", "king", "king"];
+ var cards = [
+ {
+ 	rank: "queen",
+ 	suit: "hearts",
+ 	cardImage: "images/queen-of-hearts.png"
+ },
+ {
+ 	rank: "queen",
+ 	suit: "diamonds",
+ 	cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+ 	rank: "king",
+ 	suit: "hearts",
+ 	cardImage: "images/king-of-hearts.png"
+ },
+ {
+ 	rank: "king",
+ 	suit: "diamonds",
+ 	cardImage: "images/king-of-diamonds.png"
+ }
+ ];
+
  var cardsInPlay = [];
+
  var checkForMatch = function() {
  	if (cardsInPlay[0] === cardsInPlay[1]) {
 	 		alert("You found a match!");
@@ -9,9 +32,11 @@
  }
 
  var flipCard = function (cardId) {
-	 console.log("User flipped " + cards[cardId]);
+	 console.log("User flipped " + cards[cardId].rank);
+	 console.log(cards[cardId].cardImage);
+	 console.log(cards[cardId].suit);
 
-	 cardsInPlay.push(cards[cardId]);
+	 cardsInPlay.push(cards[cardId].rank);
 
 	 if (cardsInPlay.length === 2) {
 	 	checkForMatch();
